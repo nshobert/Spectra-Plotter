af1fdc7283db4865d1672a686bbd87e6d2b2b28f
Nicki Shobert <nshobert@gmail.com>
2023-11-28 06:43:03 -0800

    buggy draft. Going to do overhaul

--- source/main.py
+++ source/main.py
@@ -8,77 +8,92 @@ import plotly.graph_objects as go
 import scraper
 import functions
 
-# SIDEBAR: latitude and longitude inputs.
-lat = st.sidebar.text_input('latitude (e.g. 47.56)')
-lon = st.sidebar.text_input('longitude (e.g. -122.01)')
-
-# SIDEBAR: show small map box to confirm location.
-# set default location to seattle
-# when lat lon entered, update map
-
-# SIDEBAR: input risk category, only 1.
-risk_category = st.sidebar.radio(
-    'Select risk category',
-    ['I', 'II', 'III', 'IV']
-    )
-
-# SIDEBAR: site class inputs, up to 9.
-site_classes = st.sidebar.multiselect(
-    'Select one or more site classes:',
-    ['A','B','BC','C','CD', 'D', 'DE', 'E', 'Default']
-    )
-
-# SIDEBAR: name the location of the project.
-title = st.sidebar.text_input('Project Location (e.g. Kirkland)')
-
-# SIDEBAR: option to create composite spectrum
-composite = st.sidebar.radio(
-    'Select to plot maximum composite spectrum:',
-     ['yes', 'no']
-     )
-
-# SIDEBAR: submit button.
-st.sidebar.button("SUBMIT")
-
-# When submitted, call scraper to
-# Construct the URLs
-urls = []
-for site_class in site_classes:
-    url = functions.construct_url(
-        lat,
-        lon,
-        risk_category,
-        site_class,
-        title
-    )
-    urls.append[url]
-
-# When URLs are ready, scrape data
-
-# populate dataframes
-
-# When spectra dfs are made, plot the retrieved spectra
-
-
 # Title for app.
 st.header('ASCE 7-22 Response Spectra Plotter')
 st.write('an application to plot multiple response spectra')
 
-# MAIN AREA: display spectra plot.
-# create dummy df for development
-my_df=pd.DataFrame(
-    [[1,3], [2,2], [3,1]],
-    columns=['X', 'Y'])
+# User input location, confirm on map
+with st.form('location'):
+    with st.sidebar:
+        # SIDEBAR: latitude and longitude inputs.
+        lat = st.text_input('Enter latitude (e.g. 47.56)')
+        lon = st.text_input('Enter longitude (e.g. -122.01)')
+        
+        submitted1 = st.form_submit_button('Check Location')
 
-# dummy figure for development
-fig = go.Figure()
-fig.add_trace(
-    go.Scatter(
-        x=my_df['X'],
-        y=my_df['Y']
+        if submitted1:
+            # Put lat and lon into a data structure.
+            loc = pd.DataFrame(
+                [[lat, lon]],
+                columns=['lat', 'lon'])
+        
+            # when lat lon submitted, update map
+            st.map(
+                data=loc,
+                zoom=5)
+'''
+# SIDEBAR: use a form to get user inputs
+with st.form('user_inputs'):
+    with st.sidebar:
+        # SIDEBAR: input risk category, only 1.
+        risk_category = st.radio(
+            'Select risk category',
+            ['I', 'II', 'III', 'IV']
+            )
+
+        # SIDEBAR: site class inputs, up to 9.
+        site_classes = st.multiselect(
+            'Select one or more site classes:',
+            ['A','B','BC','C','CD', 'D', 'DE', 'E', 'Default']
+            )
+
+        # SIDEBAR: name the location of the project.
+        title = st.text_input('Project Location (e.g. Kirkland)')
+
+        # SIDEBAR: option to create composite spectrum
+        composite = st.radio(
+            'Select to plot maximum composite spectrum:',
+            ['yes', 'no']
+            )
+
+        # SIDEBAR: submit button.
+        submitted2 = st.form_submit_button("SUBMIT")
+
+# Upon submit, get data and show plots and dataframes.
+if submitted2:
+    # Construct the URLs
+    urls = []
+    for site_class in site_classes:
+        url = functions.construct_url(
+            lat,
+            lon,
+            risk_category,
+            site_class,
+            title
         )
-)
-st.plotly_chart(fig)
+        urls.append[url]
+
+    # When URLs are ready, scrape data
+
+    # populate dataframes
+
+    # When spectra dfs are made, plot the retrieved spectra
+
+    # MAIN AREA: display spectra plot.
+    # create dummy df for development
+    my_df=pd.DataFrame(
+        [[1,3], [2,2], [3,1]],
+        columns=['X', 'Y'])
+
+    # dummy figure for development
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=my_df['X'],
+            y=my_df['Y']
+            )
+    )
+    st.plotly_chart(fig)
 
 # MAIN AREA: display datarame of spectral ordinates.
 st.write('The dataframe contains the spectral ordinates of the plotted data')
@@ -86,3 +101,4 @@ st.dataframe(my_df)
 
 # MAIN AREA: write out URLs and data source.
 st.write('The data was gathered from the USGS website using these URLs:', urls)
+'''
