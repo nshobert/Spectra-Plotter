904e22071f95ac6a27466900c3e7cb10edbd7ce4
Nicki Shobert <nshobert@gmail.com>
2023-12-11 14:53:45 -0800

    moved secrets to correct folder, added auth to app

--- source/main.py
+++ source/main.py
@@ -3,10 +3,37 @@
 
 # Import libraries
 import streamlit as st
+import hmac
 import pandas as pd
 import plotly.graph_objects as go
 import scraper
 import functions
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("Password incorrect")
+    return False
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 # Title for app.
 st.header('ASCE 7-22 Response Spectra Plotter')
